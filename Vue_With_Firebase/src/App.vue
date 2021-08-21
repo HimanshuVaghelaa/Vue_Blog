a6dd073d19b675b4b7729d3c50123c4f308b4958
Himanshu Vaghela <HimanshuVaghela2020@gmail.com>
2021-08-22 01:43:01 +0530

    Added Navigation, Footer, Home

--- Vue_With_Firebase/src/App.vue
+++ Vue_With_Firebase/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
+      <Navigation />
       <router-view />
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
+import Navigation from "./components/Navigation.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "app",
-  components: {},
+  components: {
+    Navigation,
+    Footer,
+  },
   data() {
     return {};
   },
@@ -50,5 +57,18 @@ export default {
 
 .link-light {
   color: #fff;
+}
+
+.arrow {
+  margin-left: 8px;
+  width: 12px;
+  path {
+    fill: #000;
+  }
+}
+.arrow-light {
+  path {
+    fill: #fff;
+  }
 }
 </style>
---
+++ Vue_With_Firebase/src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY_VALUE,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID_VALUE,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp };
+export default firebaseApp.firestore();
