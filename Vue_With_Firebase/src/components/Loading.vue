b88dfb58325a024c420797e7bb1d998d6c3385be
Himanshu Vaghela <HimanshuVaghela2020@gmail.com>
2021-08-28 19:02:21 +0530

    Added Login, Register, ForgotPassword View, and Loading, Modal Components And Improvements

---
+++ Vue_With_Firebase/src/components/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="loading">
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+};
+</script>
+
+<style lang="scss">
+@import "../assets/styles/Loading.scss";
+</style>
--- Vue_With_Firebase/src/views/Home.vue
+++ Vue_With_Firebase/src/views/Home.vue
@@ -30,8 +30,8 @@
 </template>
 
 <script>
-import BlogPost from "../components/BlogPost.vue";
-import BlogCard from "../components/BlogCard.vue";
+import BlogPost from "../components/BlogPost";
+import BlogCard from "../components/BlogCard";
 
 export default {
   name: "Home",
@@ -67,46 +67,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.blog-card-wrap {
-  h3 {
-    font-weight: 300;
-    font-size: 28px;
-    margin-bottom: 32px;
-  }
-}
-
-.updates {
-  .container {
-    padding: 100px 25px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    @media (min-width: 800px) {
-      padding: 125px 25px;
-      flex-direction: row;
-    }
-
-    .router-button {
-      display: flex;
-      font-size: 14px;
-      text-decoration: none;
-      @media (min-width: 800px) {
-        margin-left: auto;
-      }
-    }
-
-    h2 {
-      font-weight: 300;
-      font-size: 32px;
-      max-width: 425px;
-      width: 100%;
-      text-align: center;
-      text-transform: uppercase;
-      @media (min-width: 800px) {
-        text-align: initial;
-        font-size: 40px;
-      }
-    }
-  }
-}
+@import "../assets/styles/Home.scss";
 </style>
